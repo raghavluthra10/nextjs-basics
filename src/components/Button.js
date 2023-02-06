b9292186dc428bf6f159394e9a2b22ba369c343f
Raghav Luthra <raghavluthra1@gmail.com>
2023-02-06 19:04:51 +0530

    added Button, If component and renamed Layout component to page

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Button({ onClick, children }) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "@/styles/Home.module.css";
 
-function Layout({ children }) {
+function Page({ children }) {
   return <div className="px-4 py-4 mt-6">{children}</div>;
 }
 
-export default Layout;
+export default Page;
