2b0c046792f8d3411dc8c0335d96ed4eeb690804
Raghav Luthra <raghavluthra1@gmail.com>
2023-02-07 18:25:06 +0530

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="flex items-center bg-black text-white p-4 ">
+      <div>
+        <Link className="mr-6" href="/rest">
+          Rest Api
+        </Link>
+
+        <Link href="/graphql">Graphql</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
