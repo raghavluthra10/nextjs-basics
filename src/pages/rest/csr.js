50045eb6ef23497179e4a30629c29c1b4d2ef3ee
Raghav Luthra <raghavluthra1@gmail.com>
2023-02-08 17:15:44 +0530

    rendering data on client side

--- src/pages/rest/csr.js
+++ src/pages/rest/csr.js
@@ -1,12 +1,30 @@
-import Navbar from "@/components/Navbar";
 import React from "react";
-import TabLayout from "@/components/TabLayout";
 import getRestLayout from "@/layouts/RestLayout";
+import Button from "@/components/Button";
 
 function Csr() {
+  const handleClick = async () => {
+    try {
+      const result = await fetch("/api/rest");
+      const json = await result.json();
+      console.log("result =>", json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Csr</h1>
+    <div className="p-4">
+      <h1 className="mb-2">
+        This page shows how data is fetched in next js if we want to do it on
+        the client side.
+      </h1>
+
+      <h2 className="mb-2">
+        If you are coming from React.js, you would be familiar with this way of
+        fetching data.
+      </h2>
+      <Button onClick={handleClick}>Log data</Button>
     </div>
   );
 }
