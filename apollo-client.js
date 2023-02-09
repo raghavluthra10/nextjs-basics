cc2195b6fb0efd818a7abc3f811e12cb1d1e8359
Raghav Luthra <raghavluthra1@gmail.com>
2023-02-09 20:40:33 +0530

    updated uri for graphql server

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api.spacex.land/graphql",
+  uri: "http://localhost:3000/api/graphql",
   cache: new InMemoryCache(),
 });
 
